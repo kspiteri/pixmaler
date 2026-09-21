b91dec7950c4620b3d999df154abc3df45d59665
kspiteri <contact@keithspiteri.com>
2026-09-21 15:33:21 +0200

    refactor: add disabled state to accordion items

--- src/components/elements/accordion/Item.vue
+++ src/components/elements/accordion/Item.vue
@@ -6,7 +6,7 @@ import { ChevronDown } from '@lucide/vue'
 import { computed, inject, useId } from 'vue'
 import { accordionKey } from './key'
 
-const props = defineProps<{ id: string, title: string }>()
+const props = defineProps<{ id: string, title: string, disabled?: boolean }>()
 
 const ctx = inject(accordionKey)
 const bodyId = useId()
@@ -15,11 +15,12 @@ const open = computed(() => ctx?.openId.value === props.id)
 </script>
 
 <template>
-  <div class="accordion__item" :class="{ 'is-open': open }">
+  <div class="accordion__item" :class="{ 'is-open': open, 'is-disabled': disabled }">
     <h3 class="accordion__heading">
       <button
         :id="headId"
         type="button"
+        :disabled="disabled"
         class="accordion__trigger"
         :aria-expanded="open"
         :aria-controls="bodyId"
@@ -27,7 +28,7 @@ const open = computed(() => ctx?.openId.value === props.id)
       >
         <span class="accordion__title">{{ title }}</span>
         <span v-if="!open" class="accordion__summary"><slot name="summary" /></span>
-        <ChevronDown class="accordion__chevron" :size="18" aria-hidden="true" />
+        <ChevronDown v-if="!disabled" class="accordion__chevron" :size="18" aria-hidden="true" />
       </button>
     </h3>
     <div v-show="open" :id="bodyId" class="accordion__body" role="region" :aria-labelledby="headId">
